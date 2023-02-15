892cf8e9335929d6190598334756b3c1d08fb7af
GuilhermeFMP <guilhermemoraispinho@gmail.com>
2023-02-15 00:31:39 -0300

    feat(Route): add routes and pages

---
+++ front-end-test/src/pages/AddToken.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AddToken() {
+  return(
+    <div>
+      <h1>AddToken</h1>
+    </div>
+  );
+}
+
+export default AddToken;
---
+++ front-end-test/src/pages/EditToken.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EditToken() {
+  return(
+    <div>
+      <h1>EditToken</h1>
+    </div>
+  );
+}
+
+export default EditToken;
