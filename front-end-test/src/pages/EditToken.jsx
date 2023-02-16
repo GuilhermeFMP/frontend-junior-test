390b8172f109d701cd7d9c2d70ef8fda10ed9f0e
GuilhermeFMP <guilhermemoraispinho@gmail.com>
2023-02-16 03:14:15 -0300

    feat(pages): Add inputs, buttons and css

--- front-end-test/src/pages/EditToken.jsx
+++ front-end-test/src/pages/EditToken.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from '../components/Form';
 import Header from '../components/Header';
 
 function EditToken() {
   return(
     <div>
       <Header />
+      <Form name='Edit Token' remove />
     </div>
   );
 }
